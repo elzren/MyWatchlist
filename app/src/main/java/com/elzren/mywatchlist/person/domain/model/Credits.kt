7044c55077e9e24009790334bc224d7e202d686c
elzren <elzren00@gmail.com>
2025-12-11 09:53:39 +0530

    fix ClassCastException

--- app/src/main/java/com/elzren/mywatchlist/person/domain/model/Credits.kt
+++ app/src/main/java/com/elzren/mywatchlist/person/domain/model/Credits.kt
@@ -1,6 +1,10 @@
 package com.elzren.mywatchlist.person.domain.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Credits<T>(
+    @SerializedName("cast")
     val cast: List<T>,
+    @SerializedName("id")
     val id: Int
 )
